4c87d23dee1668b5c975433e3bc821919f1e91cf
Jason Weathered <git@jason.backtick.org>
2010-06-04 14:41:18 +1000

    Create .bashrc link as well as .bash_profile in dotfiles installer.

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 cd
+
 [ -d .dotfiles ] || git clone git://github.com/jasoncodes/dotfiles.git .dotfiles
 ( cd .dotfiles && git pull --stat )
-find .dotfiles -maxdepth 1 -type f -not -name 'install.sh' -not -name 'README*' | while read SRC
-do
-	DST="`echo "$SRC" | sed -e 's#.*/#.#' -e 's#\.profile$#.bash_profile#'`"
+
+function create_link()
+{
+	
+	local SRC="$1"
+	local DST="$2"
+	
 	if [ ! -e "$DST" ]
 	then
 		ln -sv "$SRC" "$DST"
@@ -19,5 +24,18 @@ do
 				echo " (not a symlink)"
 			fi
 		fi
+	fi
+	
+}
+
+find .dotfiles -maxdepth 1 -type f -not -name 'install.sh' -not -name 'README*' | while read SRC
+do
+	DST="`echo "$SRC" | sed -e 's#.*/#.#'`"
+	if echo "$SRC" | grep -q /profile$
+	then
+		create_link "$SRC" .bash_profile
+		create_link "$SRC" .bashrc
+	else
+		create_link "$SRC" "$DST"
 	fi
 done
