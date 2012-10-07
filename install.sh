a5afedcad615dd5e0446ea4daa9ce177054fcf04
Jason Weathered <jason@jasoncodes.com>
2012-10-08 09:21:12 +1000

    Use Fresh. http://freshshell.com/

--- install.sh
+++ install.sh
@@ -59,3 +59,10 @@ do
       ;;
   esac
 done
+
+if [ -e ~/.fresh/build/shell.sh ]; then
+  source ~/.fresh/build/shell.sh
+  fresh
+else
+  bash -c "`curl -sL get.freshshell.com`"
+fi
--- profile
+++ profile
@@ -1,3 +1,5 @@
+source ~/.fresh/build/shell.sh
+
 # locale stuff
 export LANG=en_AU.UTF-8
 export LC_CTYPE=en_US.UTF-8
