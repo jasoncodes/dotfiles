f25519cc1452a87b2c80cf0e2c4998c5d02e8587
Jason Weathered <jason@jasoncodes.com>
2012-11-05 16:10:28 +1000

    Make TERM_PROGRAM detection zsh compatible.
    http://superuser.com/a/499815.

--- shell/editor.sh
+++ shell/editor.sh
@@ -1,7 +1,7 @@
 export EDITOR='vim'
 export BUNDLER_EDITOR='vim'
 export GEM_EDITOR='vim'
-if [[ "$TERM_PROGRAM" =~ iTerm|Apple_Terminal ]] && [[ -x "`which mvim`" ]]; then
+if echo "$TERM_PROGRAM" | egrep -q 'iTerm|Apple_Terminal' && [[ -x "$(which mvim)" ]]; then
   if [[ -z "$SSH_CONNECTION" ]] && [[ -z "$TMUX" ]]; then
     export BUNDLER_EDITOR='mvim'
     export GEM_EDITOR='mvim'
