fa81fd98baf1eb2211e3bd6755a955685922de8c
Jason Weathered <jason@jasoncodes.com>
2012-11-07 08:52:10 +1000

    Add `install-ruby` function to install new rubies.
    Based on https://gist.github.com/1223731.

---
+++ aliases/rbenv.sh
@@ -0,0 +1,25 @@
+install-ruby() {
+  local VERSION="$1"
+  if [ -z "$VERSION" ]; then
+    echo "install-ruby: Specify the version of Ruby you want to install."
+    echo
+    echo "Available rubies:"
+    ruby-build --definitions
+    return 1
+  fi
+  brew update
+  brew install rbenv ruby-build rbenv-vars readline ctags
+  if [ -n "${ZSH_VERSION:-}" ]; then
+    echo 'eval "$(rbenv init - --no-rehash)"' >> ~/.zshrc
+  else
+    echo 'eval "$(rbenv init - --no-rehash)"' >> ~/.bash_profile
+  fi
+  eval "$(rbenv init - --no-rehash)" # load rbenv in the current shell
+  CONFIGURE_OPTS="--disable-install-doc --with-readline-dir=$(brew --prefix readline)" rbenv install $VERSION
+  export RBENV_VERSION="$VERSION"
+  gem install --no-ri --no-rdoc bundler rbenv-rehash git-up hitch gem-browse gem-ctags cheat awesome_print pry
+  gem ctags
+  echo "To make $VERSION your default Ruby, run the following:"
+  echo
+  echo "    rbenv global $VERSION"
+}
