480d0e0c207e810640107e3af94ec48009b63669
Jason Weathered <jason@jasoncodes.com>
2013-01-03 10:21:29 +1000

    Always use terminal Vim.

--- shell/editor.sh
+++ shell/editor.sh
@@ -1,9 +1,1 @@
 export EDITOR='vim'
-export BUNDLER_EDITOR='vim'
-export GEM_EDITOR='vim'
-if echo "$TERM_PROGRAM" | egrep -q 'iTerm|Apple_Terminal' && [[ -x "$(which mvim)" ]]; then
-  if [[ -z "$SSH_CONNECTION" ]] && [[ -z "$TMUX" ]]; then
-    export BUNDLER_EDITOR='mvim'
-    export GEM_EDITOR='mvim'
-  fi
-fi
