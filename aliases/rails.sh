3305334e65ac0f5291bc69f96ef04bfbf4edc8aa
Jason Weathered <jason@jasoncodes.com>
2013-02-13 09:34:51 +1000

    Rename `bec` to `cucumber`. Anyone still using this? :)

--- aliases/rails.sh
+++ aliases/rails.sh
@@ -1,4 +1,4 @@
-alias bec='$([ -S .zeus.sock ] && echo zeus || echo bundle exec) cucumber -f fuubar'
+alias cucumber='$([ -S .zeus.sock ] && echo zeus || echo bundle exec) cucumber -f fuubar'
 alias cuke='$([ -S .zeus.sock ] && echo zeus || echo bundle exec) cucumber -f pretty'
 alias zs='zeus start'
 alias rc='$([ -S .zeus.sock ] && echo zeus console || echo bundle exec pry -r ./config/environment)'
