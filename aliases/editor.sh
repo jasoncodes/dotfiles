de6018f8cd2ad663a8912b3427a89160535a103f
Jason Weathered <jason@jasoncodes.com>
2013-03-12 22:42:16 +1000

    Update `editlast` to work without GNU's stat.

--- aliases/editor.sh
+++ aliases/editor.sh
@@ -1,12 +1,16 @@
 # `editlast` opens the last modified file in the editor
 editlast() {
+  if stat --version 2>/dev/null| grep -q GNU; then
+    STAT_OPTION='-c'
+    STAT_FORMAT='%Y %n'
+  else
+    STAT_OPTION='-f'
+    STAT_FORMAT='%m %N'
+  fi
   FILE="$(
-    /usr/bin/find "${1:-.}" -type f \
-      -not -regex '\./\..*' \
-      -not -regex '\./tmp/.*' \
-      -not -regex '\./log/.*' \
-      -exec stat -c '%Y %n' {} +\; |
-    sort -n | tail -1 | cut -d ' ' -f 2-
+    git ls-files --cached --other --exclude-standard "$@" -z |
+      xargs -0 stat "$STAT_OPTION" "$STAT_FORMAT" |
+      sort -n | tail -1 | cut -d ' ' -f 2-
   )"
   "${EDITOR:-vi}" "$FILE"
 }
