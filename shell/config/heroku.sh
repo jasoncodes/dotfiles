87e763b9034b2851f6b62ca123b7c911a2f5bbf6
Jason Weathered <jason@jasoncodes.com>
2013-04-03 15:20:27 +1000

    Move out rbenv and Heroku Toolbelt path config.
    These load in a specific order to ensure Heroku has preference in PATH.

---
+++ shell/config/heroku.sh
@@ -0,0 +1,4 @@
+# initialize Heroku Toolbelt
+if [[ -d "/usr/local/heroku/bin" ]]; then
+  export PATH="/usr/local/heroku/bin:$PATH"
+fi
---
+++ shell/config/rbenv.sh
@@ -0,0 +1,5 @@
+# initialise rbenv
+if which rbenv &> /dev/null; then
+  eval "$(rbenv init - --no-rehash)"
+fi
+
