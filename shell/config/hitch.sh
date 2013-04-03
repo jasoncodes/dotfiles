684fb8f934149620180cb975404419f2ed6a677e
Jason Weathered <jason@jasoncodes.com>
2013-04-03 15:26:57 +1000

    Move hitch config out of the common profile file.

---
+++ shell/config/hitch.sh
@@ -0,0 +1,6 @@
+# http://github.com/therubymug/hitch
+hitch() {
+  command hitch "$@"
+  if [[ -s "$HOME/.hitch_export_authors" ]] ; then source "$HOME/.hitch_export_authors" ; fi
+}
+alias unhitch='hitch -u'
