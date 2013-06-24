778cda4b9cc691473782251786087d782442cd2a
Jason Weathered <jason@jasoncodes.com>
2013-06-25 08:56:29 +1000

    Move SSH cert environment into rbenv.sh
    Because the only reason I have this set is for Ruby.

--- shell/config/rbenv.sh
+++ shell/config/rbenv.sh
@@ -3,3 +3,7 @@ if which rbenv &> /dev/null; then
   eval "$(rbenv init - --no-rehash)"
 fi
 
+# use our own SSL root certificate if present
+if [[ -z "$SSL_CERT_FILE" ]] && [[ -f "/usr/local/etc/openssl/certs/cert.pem" ]]; then
+  export SSL_CERT_FILE="/usr/local/etc/openssl/certs/cert.pem"
+fi
