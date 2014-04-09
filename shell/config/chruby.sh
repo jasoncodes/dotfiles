c29edf47971c405ff2548bb45d9f14e2f103afdd
Jason Weathered <jason@jasoncodes.com>
2014-04-09 16:46:33 +1000

    Switch from rbenv to chruby

---
+++ shell/config/chruby.sh
@@ -0,0 +1,12 @@
+# brew install chruby ruby-install
+# ruby-install ruby 2.1.1
+# echo 2.1.1 > ~/.ruby-version
+# gem install bundler pry awesome_print git-up hitch gem-browse gem-ctags
+
+CHRUBY_PATH=/usr/local/opt/chruby/share/chruby
+if [ -d "$CHRUBY_PATH" ]; then
+  source "$CHRUBY_PATH/chruby.sh"
+  source "$CHRUBY_PATH/auto.sh"
+fi
+
+export BUNDLE_JOBS=4
