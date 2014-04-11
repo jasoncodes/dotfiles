890d6cc8bda792fb0d789a76687868883c8b4ec9
Jason Weathered <jason@jasoncodes.com>
2014-04-11 10:11:09 +1000

    Silence direnv's loading/export/unloading messages

--- shell/config/direnv.sh
+++ shell/config/direnv.sh
@@ -1,3 +1,14 @@
-if which direnv &> /dev/null; then
-  eval "$(direnv hook $0)"
+_direnv_hook() {
+  eval "$(
+    (
+      (
+        direnv export bash 3>&1 1>&2 2>&3 3>&- |
+          egrep -v -e '^direnv: (loading|export|unloading)'
+      ) 3>&1 1>&2 2>&3 3>&-
+    )
+  )"
+};
+
+if ! [[ "$PROMPT_COMMAND" =~ _direnv_hook ]]; then
+  PROMPT_COMMAND="_direnv_hook;$PROMPT_COMMAND";
 fi
