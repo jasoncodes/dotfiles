a91536cb58d896eae06402209a4d68b210cbd738
Jason Weathered <jason@jasoncodes.com>
2014-04-20 06:20:06 +1000

    Don't error if direnv is not installed

--- shell/config/direnv.sh
+++ shell/config/direnv.sh
@@ -1,14 +1,16 @@
-_direnv_hook() {
-  eval "$(
-    (
+if which direnv &> /dev/null; then
+  _direnv_hook() {
+    eval "$(
       (
-        direnv export bash 3>&1 1>&2 2>&3 3>&- |
-          egrep -v -e '^direnv: (loading|export|unloading)'
-      ) 3>&1 1>&2 2>&3 3>&-
-    )
-  )"
-};
+        (
+          direnv export bash 3>&1 1>&2 2>&3 3>&- |
+            egrep -v -e '^direnv: (loading|export|unloading)'
+        ) 3>&1 1>&2 2>&3 3>&-
+      )
+    )"
+  };
 
-if ! [[ "$PROMPT_COMMAND" =~ _direnv_hook ]]; then
-  PROMPT_COMMAND="_direnv_hook;$PROMPT_COMMAND";
+  if ! [[ "$PROMPT_COMMAND" =~ _direnv_hook ]]; then
+    PROMPT_COMMAND="_direnv_hook;$PROMPT_COMMAND";
+  fi
 fi
