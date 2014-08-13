521bc5fffa8225df1670d40b53a59571b330c7cf
Jason Weathered <jason@jasoncodes.com>
2014-08-13 17:20:06 +1000

    Avoid trying to configure tty when non-interactive
    Fixes "stty: standard input: Inappropriate ioctl for device"

--- shell/config/keybindings.sh
+++ shell/config/keybindings.sh
@@ -1,6 +1,8 @@
-# ^W deletes path segments
-stty werase undef
-bind '\C-w:unix-filename-rubout'
+if [ -t 0 ]; then
+  # ^W deletes path segments
+  stty werase undef
+  bind '\C-w:unix-filename-rubout'
 
-# allow ctrl-s to work
-stty -ixon
+  # allow ctrl-s to work
+  stty -ixon
+fi
