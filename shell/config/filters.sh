3823b273b8f945fa2a96752b6ac1af8a9a57e29b
Jason Weathered <jason@jasoncodes.com>
2015-11-25 10:17:27 +1000

    Prefer yajl's json_reformat over python's json.tool
    yajl preserves the original order of properties

--- shell/config/filters.sh
+++ shell/config/filters.sh
@@ -7,7 +7,14 @@ xml() {
 }
 
 json() {
-  python -mjson.tool | if [ -t 1 ] && which coderay &> /dev/null; then
+  (
+    if which json_reformat &> /dev/null; then
+      json_reformat # yajl
+    else
+      python -mjson.tool
+    fi
+  ) |
+  if [ -t 1 ] && which coderay &> /dev/null; then
     coderay -json
   else
     cat
