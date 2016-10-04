5a38510585475865eb29a73c6b71a20b47b62429
Jason Weathered <jason@jasoncodes.com>
2016-10-05 09:19:46 +1000

    Add @arbelt's "send escape on short ctrl press" Hammerspoon config
    https://gist.github.com/arbelt/b91e1f38a0880afb316dd5b5732759f1

---
+++ hammerspoon/control_escape.lua
@@ -0,0 +1,43 @@
+ctrl_table = {
+    sends_escape = true,
+    last_mods = {}
+}
+
+control_key_timer = hs.timer.delayed.new(0.15, function()
+    ctrl_table["send_escape"] = false
+    -- log.i("timer fired")
+    -- control_key_timer:stop()
+end
+)
+
+last_mods = {}
+
+control_handler = function(evt)
+  local new_mods = evt:getFlags()
+  if last_mods["ctrl"] == new_mods["ctrl"] then
+      return false
+  end
+  if not last_mods["ctrl"] then
+    -- log.i("control pressed")
+    last_mods = new_mods
+    ctrl_table["send_escape"] = true
+    -- log.i("starting timer")
+    control_key_timer:start()
+  else
+    -- log.i("contrtol released")
+    -- log.i(ctrl_table["send_escape"])
+    if ctrl_table["send_escape"] then
+      -- log.i("send escape key...")
+      hs.eventtap.keyStroke({}, "ESCAPE")
+    end
+    last_mods = new_mods
+    control_key_timer:stop()
+  end
+  return false
+end
+
+control_tap = hs.eventtap.new({12}, control_handler)
+
+control_tap:start()
+
+
