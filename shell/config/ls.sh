3f49aa5eee4abdf7c1335f2250882ce1f63ac35f
Jason Weathered <jason@jasoncodes.com>
2017-08-05 13:41:27 +1000

    Remove deprecated GREP_OPTIONS

--- shell/config/ls.sh
+++ shell/config/ls.sh
@@ -1,7 +1,6 @@
 # I love colour
 if ls --version 2> /dev/null | grep -q 'GNU coreutils'
 then
-  export GREP_OPTIONS='--color=auto'
   alias ls="ls --color=auto --classify --block-size=\'1"
 else
   alias ls='ls -GFh'
