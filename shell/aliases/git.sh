981a94eb9442753512b7a495432ebb54f96afea1
Jason Weathered <jason@jasoncodes.com>
2017-09-04 17:39:38 +1000

    Ensure git flow branches are not automatically cleaned up

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -262,7 +262,7 @@ grb() {
 gcu() {
   if _git_assert_origin_head; then
     HEAD_NAME="$(git rev-parse --abbrev-ref origin/HEAD | sed 's/^origin\///')"
-    git branch --merged origin/HEAD | grep -v '^\*' | awk '{print $1}' | grep -Fxv "$HEAD_NAME" | xargs git branch -d
+    git branch --merged origin/HEAD | grep -v '^\*' | awk '{print $1}' | grep -Fxv -e "$HEAD_NAME" -e develop -e master | xargs git branch -d
     git branch --remotes --merged origin/HEAD | grep -v origin/HEAD | grep '^ *origin/' | sed 's#^ *origin/##' | grep -Fxv "$HEAD_NAME"
   fi
 }
