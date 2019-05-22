a1ffd1fbeb826b494b963ea244c8b3085d11c236
Jason Weathered <jason@jasoncodes.com>
2019-05-22 14:23:32 +1000

    Include stat in `gsw` output

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -1,6 +1,6 @@
 alias gl='glg $(git show-ref | cut -d " " -f 2 | grep -v stash$) HEAD'
 alias glw='glp --word-diff'
-alias gsw='git show --format=fuller'
+alias gsw='git show --format=fuller --stat --patch'
 alias gco='git checkout'
 alias gcp='git checkout -p'
 alias gs='git status --untracked-files=all'
