127cec57eac04faf55ab96ab96144ba402d494fb
Jason Weathered <jason@jasoncodes.com>
2021-10-13 08:19:04 +1000

    Add ~/.local/bin to the PATH for pipx

--- shell/config/path.sh
+++ shell/config/path.sh
@@ -1,2 +1,2 @@
 # our own bin dir at the highest priority, followed by /usr/local/bin
-export PATH=~/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
+export PATH=~/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
