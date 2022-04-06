aa0c415fd569fe8d4fc24bb69417787c71ae890f
Jason Weathered <jason@jasoncodes.com>
2022-04-06 10:57:37 +1000

    Add support for Homebrew on Apple Silicon

--- shell/config/path.sh
+++ shell/config/path.sh
@@ -1,2 +1,7 @@
 # our own bin dir at the highest priority, followed by /usr/local/bin
 export PATH=~/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
+
+# add Homebrew to the path if it's installed in /opt/homebrew
+if ! which brew &> /dev/null && [ -x /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
