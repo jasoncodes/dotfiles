87adff0a3aeed9d2cccb9a6e9ae9ac60d81c0a37
Jason Weathered <jason@jasoncodes.com>
2022-04-15 18:24:46 +1000

    Ensure eternal history is not world readable

--- shell/config/history.sh
+++ shell/config/history.sh
@@ -4,4 +4,4 @@ export HISTFILESIZE=100000
 export HISTCONTROL=ignoreboth
 export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S  "
 shopt -s histappend
-export PROMPT_COMMAND='history -a; echo "$$ $USER $(history 1)" >> ~/.bash_eternal_history'"${PROMPT_COMMAND:+;}${PROMPT_COMMAND}"
+export PROMPT_COMMAND='history -a; echo "$$ $USER $(history 1)" >> ~/.bash_eternal_history; chmod 600 ~/.bash_eternal_history'"${PROMPT_COMMAND:+;}${PROMPT_COMMAND}"
