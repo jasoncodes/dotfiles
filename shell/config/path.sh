e6c23c14f2eb6fd3cd4c80eda8f5bf9d8e09b1d4
Jason Weathered <jason@jasoncodes.com>
2023-04-10 13:42:37 +1000

    Add Homebrew libpq to PATH for PostgreSQL client tools
    This avoids having to link a key-only formula to get `psql`, etc.

--- shell/config/path.sh
+++ shell/config/path.sh
@@ -5,3 +5,10 @@ export PATH=~/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
 if ! which brew &> /dev/null && [ -x /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# add libpq to the path for PostgreSQL client tools
+homebrew_libpq_bin="${HOMEBREW_PREFIX:-/usr/local}/opt/libpq/bin"
+if [ -d "$homebrew_libpq_bin" ]; then
+  export PATH="$homebrew_libpq_bin:$PATH"
+fi
+unset homebrew_libpq_bin
