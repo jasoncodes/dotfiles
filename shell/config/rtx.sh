06c554241f7b795e09cc636e939aefa7fc1b4559
Jason Weathered <jason@jasoncodes.com>
2023-05-11 09:20:16 +1000

    Add support for rtx
    rtx is a Rust clone of asdf. It uses asdf plugins.
    
    https://github.com/jdxcode/rtx

---
+++ shell/config/rtx.sh
@@ -0,0 +1,11 @@
+# brew install jdxcode/tap/rtx
+# rtx install
+
+if command -v rtx > /dev/null; then
+  if [[ -n "${BASH_VERSION:-}" ]]; then
+    eval "$(rtx activate bash)"
+  fi
+  if [[ -n "${ZSH_VERSION:-}" ]]; then
+    eval "$(rtx activate zsh)"
+  fi
+fi
