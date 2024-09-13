cf31dcfda8a4fd90dc6a0f7390b7abd835bb92f5
Jason Weathered <jason@jasoncodes.com>
2024-09-13 15:21:58 +1000

    Use configured pager options instead of default less when grepping diffs

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -379,7 +379,7 @@ grepdiff-hunk() {
     local pattern="$1"; shift
 
     grepdiff --extended-regexp "$pattern" --only-match=modifications --output-matching=hunk |
-      (if [ -t 1 ] && command -v diff-highlight > /dev/null; then exec diff-highlight; else exec cat; fi)
+      (if [ -t 1 ] && command -v diff-highlight > /dev/null; then diff-highlight | ${PAGER:-less}; else exec cat; fi)
   )
 }
 
