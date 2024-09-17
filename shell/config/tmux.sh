4dbc5148a27aacf04f1ade7a7af31a2d84c9d10b
Jason Weathered <jason@jasoncodes.com>
2024-09-17 11:26:03 +1000

    Add ssh-tmux function to start tmux control mode on a remote host
    This works well with iTerm's built-in tmux support.

--- shell/config/tmux.sh
+++ shell/config/tmux.sh
@@ -5,3 +5,15 @@ tmux() {
     command tmux "$@"
   fi
 }
+
+# https://iterm2.com/documentation-tmux-integration.html
+ssh-tmux() {
+  if [[ $# -ne 1 ]]; then
+    echo "usage: ssh-tmux <host>" >&2
+    return 1
+  fi
+
+  local host="${1?}"
+
+  ssh -t "$host" "tmux -CC new -A -s 0"
+}
