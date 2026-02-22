1e13f99a7e84b5529688ca8bbf8007014e09325d
Jason Weathered <jason@jasoncodes.com>
2026-02-22 13:03:38 +1000

    Add Homebrew on Linux to the PATH if installed

--- shell/config/path.sh
+++ shell/config/path.sh
@@ -1,9 +1,12 @@
 # our own bin dir at the highest priority, followed by /usr/local/bin
 export PATH=~/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
 
-# add Homebrew to the path if it's installed in /opt/homebrew
+# add Homebrew to the path if it's installed
 if ! which brew &> /dev/null && [ -x /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+if ! which brew &> /dev/null && [ -x /home/linuxbrew/.linuxbrew/bin ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 # add libpq to the path for PostgreSQL client tools
